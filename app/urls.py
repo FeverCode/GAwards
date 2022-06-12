2234814b5f5c0d50606191653ebc883ded163c62
FeverCode <giddylancs.gl@gmail.com>
2022-06-12 20:24:08 +0300

    Created register form

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from app import views
 from rest_framework.routers import DefaultRouter
+from .views import RegisterView
 
 
 
@@ -14,5 +15,6 @@ router.register('profile', views.ProfileViewSet, basename="profile")
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/', include(router.urls)),
+    path('register/', RegisterView.as_view(), name='register'),
     
 ]
