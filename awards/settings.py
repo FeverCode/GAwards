e9fae3d0e3ca5b5af563b146ca1960b122293fee
FeverCode <giddylancs.gl@gmail.com>
2022-06-16 14:15:45 +0300

    Updated debug settings

--- awards/settings.py
+++ awards/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 MODE = config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG')
 # development
 if config('MODE') == "dev":
    DATABASES = {
@@ -168,7 +168,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
